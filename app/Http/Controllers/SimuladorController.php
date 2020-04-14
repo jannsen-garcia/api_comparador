7da91bde888f318ec7130f64afccfe8de3e2be80
Jannsen Garcia <jannsen.garcia@gdax.com.br>
2020-04-14 17:38:34 -0300

    first commit

---
+++ app/Http/Controllers/SimuladorController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SimuladorController extends Controller
+{
+    private $dadosSimulador;
+    private $simulacao = [];
+
+    public function simular(Request $request)
+    {
+        $this->carregarArquivoDadosSimulador()
+             ->simularEmprestimo($request->valor_emprestimo)
+        ;
+        return \response()->json($this->simulacao);
+    }
+
+    private function carregarArquivoDadosSimulador() : self
+    {
+        $this->dadosSimulador = json_decode(\File::get(storage_path("app/public/simulador/taxas_instituicoes.json")));
+        return $this;
+    }
+
+    private function simularEmprestimo(float $valorEmprestimo) : self
+    {
+        foreach ($this->dadosSimulador as $dados) {
+            $this->simulacao[$dados->instituicao][] = [
+                "taxa"            => $dados->taxaJuros,
+                "parcelas"        => $dados->parcelas,
+                "valorParcela"    => $this->calcularValorDaParcela($valorEmprestimo, $dados->coeficiente),
+                "convenio"        => $dados->convenio,
+            ];
+        }
+        return $this;
+    }
+
+    private function calcularValorDaParcela(float $valorEmprestimo, float $coeficiente) : float
+    {
+        return round($valorEmprestimo * $coeficiente, 2);
+    }
+}
